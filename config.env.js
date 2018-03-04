cdbf5f7800de8fc46955c426dfc2ffbe3e1f2199
Chris Carl Microsoft Surface Book 2 <chrisbcarl@outlook.com>
2018-03-04 14:34:53 -0500

    added temporary dummy for others to use and change

---
+++ config.env.js
@@ -0,0 +1,35 @@
+var watson = {
+    "conversation": {
+        "url": "<INSERT YOUR CREDS HERE>",
+        "username": "<INSERT YOUR CREDS HERE>",
+        "password": "<INSERT YOUR CREDS HERE>",
+        "workspace_id": "<INSERT YOUR CREDS HERE>",
+        "version_date": "<INSERT YOUR CREDS HERE>"
+    }
+};
+
+var google = {
+    "calendar": {
+        "installed": {
+            "client_id": "<INSERT YOUR CREDS HERE>",
+            "project_id": "<INSERT YOUR CREDS HERE>",
+            "auth_uri": "<INSERT YOUR CREDS HERE>",
+            "token_uri": "<INSERT YOUR CREDS HERE>",
+            "auth_provider_x509_cert_url": "<INSERT YOUR CREDS HERE>",
+            "client_secret": "<INSERT YOUR CREDS HERE>",
+            "redirect_uris": [
+            "<INSERT YOUR CREDS HERE>"
+            ]
+        }
+    }
+};
+
+var trello = {
+
+}
+
+module.exports = {
+    watson: watson,
+    google: google,
+    trello: trello
+};
